8f006fe7b796b1d27375824602554ad436ad52e4
ali hasan <ali00.hasan00@gmail.com>
2021-08-07 13:31:38 +0300

    web support

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:teaml/injectable.dart';
 import 'package:teaml/presentation/core/app_widgets.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
   runApp(const AppWidget());
 }
